d99505ae636a11f82e3bb0e5a61aa36bef46268f
Sushant <global\V867193>
2023-06-21 09:36:32 -0400

    Second Commit

--- app/src/main/java/com/sushant/sampletest/adapter/CityListAdapter.kt
+++ app/src/main/java/com/sushant/sampletest/adapter/CityListAdapter.kt
@@ -1,6 +1,5 @@
 package com.sushant.sampletest.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
--- app/src/main/java/com/sushant/sampletest/viewmodel/CityViewModel.kt
+++ app/src/main/java/com/sushant/sampletest/viewmodel/CityViewModel.kt
@@ -1,35 +1,25 @@
 package com.sushant.sampletest.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sushant.sampletest.model.CityModel
 import com.sushant.sampletest.repo.CityRepository
 import com.sushant.sampletest.util.NetworkUtil
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class CityViewModel constructor(private val cityRepository: CityRepository) : ViewModel() {
 
     val cityList = MutableLiveData<List<CityModel>>()
-    val citiesList: MutableList<CityModel> = mutableListOf()
-    //val citiesList = mutableListOf()
+    private val citiesList: MutableList<CityModel> = mutableListOf()
 
     private val cityName = mutableListOf<String>( "Visakhapatnam" , "Hyderabad" , "Pune" , "Delhi" , "Paris")
 
     fun getCities() {
-        Log.d("Thread Outside", Thread.currentThread().name)
-
         viewModelScope.launch {
-            Log.d("Thread Inside", Thread.currentThread().name)
             cityName.forEach {
                 when (val response = cityRepository.getCityDetails("520916eb3f46442ca1c12926221402", it)) {
                     is NetworkUtil.Success -> {
-                        Log.d("getCities %d", response.toString())
-                        //cityList.add
-                        //cityList.postValue(listOf(response.data))
                         citiesList.add((response.data))
                     }
                     is NetworkUtil.Error -> {
@@ -39,7 +29,6 @@ class CityViewModel constructor(private val cityRepository: CityRepository) : Vi
                     }
                 }
             }
-            Log.d("Cities list", citiesList.count().toString())
             cityList.postValue(citiesList)
         }
     }
